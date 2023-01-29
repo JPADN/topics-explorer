4cb1a78d1b8327c2b23db4e168d4f01aedb96e85
JPADN <jpadnalemao@gmail.com>
2023-01-29 18:11:41 -0300

    Fix in build_run_prod

--- build_run_prod.sh
+++ build_run_prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd topics-explorer-frontend
-sudo docker build . -t topics-explorer-frontend
+sudo docker build -f Dockerfile.prod -t topics-explorer-frontend .
 cd ..
 cd topics-explorer-backend
-sudo docker build . -t topics-explorer-backend
+sudo docker build -f Dockerfile.prod -t topics-explorer-backend .
 cd ..
 cd prod-compose
 sudo docker-compose up
