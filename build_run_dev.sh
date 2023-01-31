4b06f4f9012fdbeb4f22412ebbcf3e952c6e01b6
JPADN <jpadnalemao@gmail.com>
2023-01-31 13:42:34 -0300

    Minor changes in scripts, and updating submodules

--- build_run_dev.sh
+++ build_run_dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd topics-explorer-frontend
-sudo docker build -f Dockerfile.dev -t topics-explorer-frontend-dev .
+docker build -f Dockerfile.dev -t topics-explorer-frontend-dev .
 cd ..
 cd topics-explorer-backend
-sudo docker build -f Dockerfile.dev -t topics-explorer-backend-dev .
+docker build -f Dockerfile.dev -t topics-explorer-backend-dev .
 cd ..
 cd dev-compose
-sudo docker-compose up
+docker-compose up
